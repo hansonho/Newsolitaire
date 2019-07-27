d67adc912d0c16766fd985fa56dfa987d90ada5a
BoHanHo <hansonho61004@gmail.com>
2019-07-28 00:15:54 +0800

    web

---
+++ precache-manifest.2089cf61d7b786b634fef4a5cfe267b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc234bc0879f36385cedc5e74f5f55c",
+    "url": "/Newsolitaire/index.html"
+  },
+  {
+    "revision": "bfb2b090698b8627a86f",
+    "url": "/Newsolitaire/static/css/main.babbe2f1.chunk.css"
+  },
+  {
+    "revision": "416ceac31df32012e442",
+    "url": "/Newsolitaire/static/js/2.589bcbbd.chunk.js"
+  },
+  {
+    "revision": "bfb2b090698b8627a86f",
+    "url": "/Newsolitaire/static/js/main.6997ab55.chunk.js"
+  },
+  {
+    "revision": "0d7d556abc824352e486",
+    "url": "/Newsolitaire/static/js/runtime~main.155f8534.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/newsolitaire/precache-manifest.7dc93f647f38f1728759f0a990fa3d8a.js"
+  "/Newsolitaire/precache-manifest.2089cf61d7b786b634fef4a5cfe267b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/newsolitaire/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Newsolitaire/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
