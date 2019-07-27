41043c8d0aedf454ef997fd0602889689729997f
BoHanHo <hansonho61004@gmail.com>
2019-07-28 00:08:10 +0800

    web

---
+++ precache-manifest.a2bf721cb876c00f47e0c3ba493e2cce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99fac2876e0fdc1d8aed604d417f0b9c",
+    "url": "/newsolitaire/index.html"
+  },
+  {
+    "revision": "5a7cf4759721f0a5ed0e",
+    "url": "/newsolitaire/static/css/main.babbe2f1.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/newsolitaire/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "5a7cf4759721f0a5ed0e",
+    "url": "/newsolitaire/static/js/main.02da9921.chunk.js"
+  },
+  {
+    "revision": "c819ea00745485b0365c",
+    "url": "/newsolitaire/static/js/runtime~main.037889da.js"
+  }
+]);
